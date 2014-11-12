8917becf217d3a9d9dcccc2a5673e4249e025077
Iain <Iain.V.Majer@gmail.com>
2014-11-12 11:37:55 +0000

    Implemented Ticket screens Layout and Tabs. Created layout for Displaying a single Bus's info

--- app/src/main/java/com/example/iain/busapp/fragment/TicketFragment.java
+++ app/src/main/java/com/example/iain/busapp/fragment/TicketFragment.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
 
 import com.example.iain.busapp.MainActivity;
 import com.example.iain.busapp.R;
+import com.example.iain.busapp.adapter.favListAdapter;
+
 
 /**
  * Created by iain on 11/11/14.
@@ -19,7 +26,6 @@ public class TicketFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
-
     /**
      * Returns a new instance of this fragment for the given section
      * number.
@@ -39,6 +45,12 @@ public class TicketFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_ticket, container, false);
+
+        createTabs(rootView);
+        populateFavList(rootView);
+        populateRecentList(rootView);
+        populateCloseList(rootView);
+
         return rootView;
     }
 
@@ -47,5 +59,108 @@ public class TicketFragment extends Fragment {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    public void createTabs(View rootView){
+        TabHost tabHost = (TabHost) rootView.findViewById(R.id.ticketTabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1, spec2, spec3;
+
+        spec1 = tabHost.newTabSpec("spec1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator(getString(R.string.ticket_tab1_text));
+        tabHost.addTab(spec1);
+
+        spec2 = tabHost.newTabSpec("spec2");
+        spec2.setContent(R.id.tab2);
+        spec2.setIndicator(getString(R.string.ticket_tab2_text));
+        tabHost.addTab(spec2);
+
+        spec3 = tabHost.newTabSpec("spec3");
+        spec3.setContent(R.id.tab3);
+        spec3.setIndicator(getString(R.string.ticket_tab3_text));
+        tabHost.addTab(spec3);
+
+
+    }
+
+    public void populateFavList(View rootView){
+        String[] favsTitles = {"Bus Number 1", "Bus Number 2", "Bus Number 3", "Bus Number 4",
+                "Bus Number 5" , "Bus Number 6" , "Bus Number 7" , "Bus Number 8" };
+        String[] favsDescs = {"Bus1 Status", "Bus2 Status", "Bus3 Status", "Bus Doesn\'t Exist",
+                "Bus5 Status", "Bus6 Status", "Bus7 Status", "Bus Doesn\'t Exist"};
+
+        favListAdapter theAdapter = new favListAdapter(this.getActivity(), favsTitles, favsDescs);
+
+        // ListViews display data in a scrollable list
+        ListView theListView = (ListView) rootView.findViewById(R.id.ticketFavList);
+
+        // Tells the ListView what data to use
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String tvShowPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(i));
+
+                Toast.makeText(TicketFragment.this.getActivity(), tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void populateRecentList(View rootView){
+        String[] recTitles = {"Bus Number 1", "Bus Number 2", "Bus Number 3", "Bus Number 4",
+                "Bus Number 5" , "Bus Number 6" , "Bus Number 7" , "Bus Number 8" };
+        String[] recDescs = {"Bus1 Status", "Bus2 Status", "Bus3 Status", "Bus Doesn\'t Exist",
+                "Bus5 Status", "Bus6 Status", "Bus7 Status", "Bus Doesn\'t Exist"};
+
+        favListAdapter theAdapter = new favListAdapter(this.getActivity(), recTitles, recDescs);
+
+        // ListViews display data in a scrollable list
+        ListView theListView = (ListView) rootView.findViewById(R.id.ticketRecentList);
+
+        // Tells the ListView what data to use
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String tvShowPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(i));
+
+                Toast.makeText(TicketFragment.this.getActivity(), tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void populateCloseList(View rootView){
+        String[] recTitles = {"Bus Number 1", "Bus Number 2", "Bus Number 3", "Bus Number 4",
+                "Bus Number 5" , "Bus Number 6" , "Bus Number 7" , "Bus Number 8" };
+        String[] recDescs = {"Bus1 Status", "Bus2 Status", "Bus3 Status", "Bus Doesn\'t Exist",
+                "Bus5 Status", "Bus6 Status", "Bus7 Status", "Bus Doesn\'t Exist"};
+
+        favListAdapter theAdapter = new favListAdapter(this.getActivity(), recTitles, recDescs);
+
+        // ListViews display data in a scrollable list
+        ListView theListView = (ListView) rootView.findViewById(R.id.ticketCloseList);
+
+        // Tells the ListView what data to use
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String tvShowPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(i));
+
+                Toast.makeText(TicketFragment.this.getActivity(), tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
